4f2300ff614cb9f873946ab60b7ef0a0bd8634b0
vv4t <watashidesu6969@gmail.com>
2025-08-04 20:37:06 +1000

    proof generation optimisation

--- cpp/src/util.h
+++ cpp/src/util.h
@@ -12,7 +12,7 @@ using namespace NTL;
 extern void BIG_from_ZZ(BIG big, const ZZ& value);
 extern ZZ ZZ_from_BIG(const BIG big);
 extern ZZ_pX polyfit(vector<pair<ZZ_p, ZZ_p>>& points);
-extern ZZ_pX from_linear_roots(vector<pair<ZZ_p, ZZ_p>>& points);
-extern void create_points_from_string(vector<pair<ZZ_p, ZZ_p>>& res, string data, int offset);
+extern void linear_roots_and_polyfit(ZZ_pX& result, ZZ_pX& linear_roots, vector<pair<ZZ_p, ZZ_p>>& points);
+extern void evaluate_polynomial_points(vector<pair<ZZ_p, ZZ_p>>& points, const ZZ_pX& poly, int start, int length);
 
 #endif
