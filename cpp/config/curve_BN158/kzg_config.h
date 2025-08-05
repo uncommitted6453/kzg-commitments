ec848d37a1661d2ffaf3aca746c28662583e8076
vv4t <watashidesu6969@gmail.com>
2025-08-05 23:03:32 +1000

    compile with different elliptic curves

---
+++ cpp/config/curve_BN158/kzg_config.h
@@ -0,0 +1,15 @@
+#ifndef KZG_CONFIG_H
+#define KZG_CONFIG_H
+
+#include <ecp_BN158.h>
+#include <ecp2_BN158.h>
+#include <fp12_BN158.h>
+#include <pair_BN158.h>
+#include <big_B160_56.h>
+
+using namespace BN158;
+using namespace BN158_BIG;
+
+#define MODBYTES_CURVE MODBYTES_B160_56
+
+#endif
---
+++ cpp/config/curve_BN254/kzg_config.h
@@ -0,0 +1,15 @@
+#ifndef KZG_CONFIG_H
+#define KZG_CONFIG_H
+
+#include <ecp_BN254.h>
+#include <ecp2_BN254.h>
+#include <fp12_BN254.h>
+#include <pair_BN254.h>
+#include <big_B256_56.h>
+
+using namespace BN254;
+using namespace BN254_BIG;
+
+#define MODBYTES_CURVE MODBYTES_B256_56
+
+#endif
