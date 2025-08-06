b078a0e56622b033e4286bbf1b7ff7f07031a034
lexsai <64449843+lexsai@users.noreply.github.com>
2025-08-06 06:32:01 +0000

    Deploying to gh-pages from @ uncommitted6453/kzg-commitments@8b65fd46f094e348ee4eb659c62891c2b12cacab 🚀

---
+++ search/classes_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['poly_0',['poly',['../classkzg_1_1poly.html',1,'kzg']]],
+  ['proof_1',['proof',['../classkzg_1_1proof.html',1,'kzg']]]
+];
---
+++ search/functions_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['trusted_5fsetup_0',['trusted_setup',['../classkzg_1_1trusted__setup.html#a97856a36959ce53d238cc273edca9b50',1,'kzg::trusted_setup::trusted_setup(int num_coeff)'],['../classkzg_1_1trusted__setup.html#a8d070d6ba980422341fda6067d0472a9',1,'kzg::trusted_setup::trusted_setup(const std::string &amp;filename)']]]
+];
