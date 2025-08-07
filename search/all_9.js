8fda40a9131433a8eb832dc23ef580fca4b2721d
lexsai <lexsai@users.noreply.github.com>
2025-08-07 05:36:54 +0000

    deploy: 99362b18212e9fd5bdbec2ffd664ba2b02d55cb7

--- search/all_9.js
+++ search/all_9.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['trusted_5fsetup_0',['trusted_setup',['../classkzg_1_1trusted__setup.html',1,'kzg::trusted_setup'],['../classkzg_1_1trusted__setup.html#a97856a36959ce53d238cc273edca9b50',1,'kzg::trusted_setup::trusted_setup(int num_coeff)'],['../classkzg_1_1trusted__setup.html#a8d070d6ba980422341fda6067d0472a9',1,'kzg::trusted_setup::trusted_setup(const std::string &amp;filename)']]]
+  ['trusted_5fsetup_0',['trusted_setup',['../classkzg_1_1trusted__setup.html',1,'kzg::trusted_setup'],['../classkzg_1_1trusted__setup.html#a3c0ac4a58f38809a78a28f5f7cafc22c',1,'kzg::trusted_setup::trusted_setup(int num_coeff, int num_secret_bytes=0)'],['../classkzg_1_1trusted__setup.html#a8d070d6ba980422341fda6067d0472a9',1,'kzg::trusted_setup::trusted_setup(const std::string &amp;filename)']]]
 ];
